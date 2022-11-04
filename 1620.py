47fcc820494d061df96ea831b847afe56849c1ba
fault2000 <gkxodnrcjswo@o.cnu.ac.kr>
2022-11-05 05:12:13 +0900

    write

--- 1620.py
+++ 1620.py
@@ -1,11 +1,14 @@
 import sys
-from collections import Counter
 
 N, M = map(int, sys.stdin.readline().strip().split(" "))
-arr = []
+arr = dict()
 
-for i in range(N):
-    arr.append([i, sys.stdin.readline().strip()])
+for i in range(1, N+1):
+    arr[i]= sys.stdin.readline().strip()
 
-arr2 = Counter(arr).get("Bulbasaur")
-
+for i in range(M):
+    input = sys.stdin.readline().strip()
+    if input.isdigit():
+        print(arr[int(input)])
+    else:
+        print(next((index for index, name in arr.items() if name == input), None))
